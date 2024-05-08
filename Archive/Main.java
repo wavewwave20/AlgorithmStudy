9ea88d752d9e1618244d3dec7504c6e106a90308
wavewwave20 <wavewwave20@gmail.com>
2024-05-08 15:05:35 +0900

    stack and queue

--- Archive/Main.java
+++ Archive/Main.java
@@ -8,150 +8,68 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int m;
-    static char[][] board;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
+    static int N,M,V;
 
-    static class PairBeads {
-        int redX;
-        int redY;
-        int blueX;
-        int blueY;
-        int cnt;
-        public PairBeads(int redX, int redY, int blueX, int blueY, int cnt) {
-            this.redX = redX;
-            this.redY = redY;
-            this.blueX = blueX;
-            this.blueY = blueY;
-            this.cnt = cnt;
+    static int[][] graph = new int[1001][1001];
+
+    static boolean[] visited = new boolean[1001];
+
+    static Queue<Integer> q = new LinkedList<>();
+
+    static void dfs(int current) {
+        if(visited[current]) {
+            return;
+        }
+        System.out.print("" + current + " ");
+        visited[current] = true;
+
+        for (int i = 1; i<1001; i++) {
+            if(graph[current][i] == 1) {
+                dfs(i);
+            }
         }
     }
 
+    static void bfs(int start) {
+        q.offer(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()) {
+            int current = q.poll();
+            System.out.print("" + current + " ");
+
+            for (int i = 1; i<1001; i++) {
+                if(graph[current][i] == 1) {
+                    if (!visited[i]) {
+                        visited[i] = true;
+                        q.offer(i);
+                    }
+                }
+            }
+        }
+    }
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        board = new char[n][m];
-
-        for ( int i = 0; i<n; i++) {
-            board[i] = br.readLine().toCharArray();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        
+        for (int i = 0 ; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = 1;
+            graph[b][a] = 1;
         }
 
-        PairBeads pb = new PairBeads(0,0,0,0,0);
+        dfs(V);
 
-        for (int i = 0; i<n; i++) {
-            for (int j = 0; j<m; j++) {
-                if(board[i][j] == 'R') {
-                    pb.redX = i;
-                    pb.redY = j;
-                    board[i][j] = '.';
-                }
-                if(board[i][j] == 'B') {
-                    pb.blueX = i;
-                    pb.blueY = j;
-                    board[i][j] = '.';
-                }
-            }
-        }
+        visited = new boolean[1001];
+        System.out.println();
+        bfs(V);
 
-        System.out.println(bfs(pb));
-	}
-
-    static int bfs(PairBeads pb) {
-        Queue<PairBeads> queue = new LinkedList<>();
-        queue.offer(pb);
-
-        while(!queue.isEmpty()) {
-            PairBeads beads = queue.poll();
-
-            if (beads.cnt == 10) {
-                continue;
-            }
-
-            for (int d = 0; d<4; d++) {
-                int redX = beads.redX;
-                int redY = beads.redY;
-                int blueX = beads.blueX;
-                int blueY = beads.blueY;
-                boolean isRedHole = false;
-                boolean isBlueHole = false;
-
-                while (true) {
-                    int newRedX = redX + dx[d];
-                    int newRedY = redY + dy[d];
-                    if(board[newRedX][newRedY] == '#') {
-                        break;
-                    }
-
-                    if(board[newRedX][newRedY] == 'O') {
-                        isRedHole = true;
-                        break;
-                    }
-                    redX = newRedX;
-                    redY = newRedY;
-                }
-
-                while (true) {
-                    int newBlueX = blueX + dx[d];
-                    int newBlueY = blueY + dy[d];
-                    if(board[newBlueX][newBlueY] == '#') {
-                        break;
-                    }
-
-                    if(board[newBlueX][newBlueY] == 'O') {
-                        isRedHole = true;
-                        break;
-                    }
-                    blueX = newBlueX;
-                    blueY = newBlueY;
-                }
-
-                if (isBlueHole) {
-                    continue;
-                } else if (isRedHole) {
-                    return beads.cnt + 1;
-                }
-                  if (beads.redX == redX && beads.redY == redY && beads.blueX == blueX && beads.blueY == blueY) {
-                    continue;
-                }
-
-                if (redX == blueX && redY == blueY) {
-                    if (d == 0) {
-                        if (beads.redX < beads.blueX) {
-                            redX--;
-                        } else {
-                            blueX--;
-                        }
-                    } else if (d == 1) {
-                        if (beads.redX < beads.blueX) {
-                            blueX++;
-                        } else  {
-                            redX++;
-                        }
-                    } else if (d == 2) {
-                        if (beads.redY < beads.blueY) {
-                            redY--;
-                        } else {
-                            blueY--;
-                        }
-                    } else if (d == 3) {
-                        if (beads.redY < beads.blueY) {
-                            blueY++;
-                        } else {
-                            redY++;
-                        }
-                    }
-                }
-                queue.offer(new PairBeads(redX, redY, blueX, blueY, beads.cnt + 1));
-            }
-        }
-
-        return -1;
     }
 }
