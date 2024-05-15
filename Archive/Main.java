5c15a9dd8115ca932e41714a11da42391d078b86
wavewwave20 <wavewwave20@gmail.com>
2024-05-15 22:12:33 +0900

    2024 풀이중

--- Archive/Main.java
+++ Archive/Main.java
@@ -1,1 +1,20 @@
 package Archive;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int [][] board = new int[n][n];
+
+        for (int i = 0; i< n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j<n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }   
+}
