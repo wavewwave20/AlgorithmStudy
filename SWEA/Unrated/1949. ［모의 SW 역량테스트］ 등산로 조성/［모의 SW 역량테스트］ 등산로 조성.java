ba8e910ced55d3f967f89db7ec01f300667877e0
박하민 <129127559+wavewwave20@users.noreply.github.com>
2024-08-10 22:56:37 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 163 ms, Memory: 20,980 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -21,6 +21,10 @@ class Solution
     static int longestWay;
     static Queue<Point> queue;
     static ArrayList<Point> startingPoints;
+
+    //dfs용 최대 길이
+    static int longestWayForDFS;
+
 	public static void main(String args[]) throws Exception
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,7 +40,7 @@ class Solution
             int[][] map = new int[N][N];
             startingPoints = new ArrayList<>();
 
-            
+            //가장 높은 봉우리 표시위한 변수
             int tmpMax= 0;
             for (int y = 0; y<N; y++) {
                 st = new StringTokenizer(br.readLine());
@@ -51,6 +55,7 @@ class Solution
                 }
             }
 
+            //가장 높은 봉우리들을 스타팅 포인트로 리스트에 저장
             for (int y = 0; y<N; y++) {
                 for (int x= 0; x<N; x++) {
                     if(map[y][x] == tmpMax) {
@@ -59,11 +64,10 @@ class Solution
                 }
             }
 
-
+            //가장긴 등산로 구하기
             longestWay = 0;
             for(int i = 0; i<N; i++) {
                 for(int j = 0; j<N; j++) {
-                    
                     makeWay(i, j, K, map);
                 }
             }
@@ -74,7 +78,11 @@ class Solution
         System.out.println(sb.toString());
 	}
 
-
+    /* 등산로 제작 함수
+     * 등산로를 제작하기위해 K만큼 깎을 수 있다.
+     * 이때에 한 점 x,y에서 높이를 변경하는 것은 상하좌우에서 갈 수 있도록 -1만큼 깎는 것을 의미한다.
+     * 따라서 nx,ny와 비교하여 높이를 변경하거나 변경하지 않고 최단거리를 구한다.
+     */
     static void makeWay(int x, int y,int k, int[][]map) {
         for(int i = 0; i<4; i++) {
             int nx = x + dx[i];
@@ -89,7 +97,7 @@ class Solution
             if(map[ny][nx] == map[y][x]) {
                 //xy 작게
                 map[y][x] -=1;
-                int result = BFS(map);
+                int result = forDFS(map);
                 if(longestWay < result) {
                     longestWay = result;
                 }
@@ -98,7 +106,7 @@ class Solution
                 //본인이 더 큰경우
             }else if (map[ny][nx] < map[y][x]) {
                 //그대로
-                int result = BFS(map);
+                int result = forDFS(map);
                 if(longestWay < result) {
                     longestWay = result;
                 }
@@ -107,7 +115,7 @@ class Solution
                 //본인이 더 크고, 본인에서 k를 뺐을때 옆보다 작아진다면 
                 if(map[y][x] -k < map[ny][nx]) {
                     map[y][x] = map[ny][nx]-1;
-                    int result2 = BFS(map);
+                    int result2 = forDFS(map);
                     if(longestWay < result2) {
                         longestWay = result2;
                     }
@@ -116,7 +124,7 @@ class Solution
 
             }else if (map[ny][nx] > map[y][x]) {
                 //그대로
-                int result = BFS(map);
+                int result = forDFS(map);
                 if(longestWay < result) {
                     longestWay = result;
                 }
@@ -129,6 +137,8 @@ class Solution
 
 
     //최대길이 등산로 반환
+    //사실 최대길이는 BFS로 구하면 안됨, BFS는 최단거리를 보장하지만 최장거리는 알 수 없음
+    //다만, 이 문제의 경우 뒤로 돌아갈 수 없기 때문에 가능하다.
     static int BFS(int[][]map) {
         int N = map.length;
         int looong = 0;
@@ -166,12 +176,42 @@ class Solution
                 }
             
         }
-
-
         return looong;
+    }
+
+    static int forDFS(int[][]map) {
+        int N = map.length;
+        longestWayForDFS = 0;
+        for (Point start : startingPoints) {
+            DFS(start.x, start.y, N, 1, map);
+        }
+        return longestWayForDFS;
+    }
+    
+    
+    static void DFS(int x, int y, int N, int d, int[][]map) {
+        
+        for(int i = 0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx<0 || ny<0|| nx>=N|| ny>=N) {
+                continue;
+            }
+            if((map[ny][nx] < map[y][x]) ) {
+                DFS(nx, ny, N, d+1, map);
+            } 
+        }
+
+        if(longestWayForDFS < d) {
+            longestWayForDFS = d;
+        }
     }
 }
     
+
+
+
 /*
 
 문제 조건 잘못 이해함
