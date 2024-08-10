75ab5dd520138cfc888ed6919d898ffa3cef0341
wavewwave20 <wavewwave20@gmail.com>
2024-08-11 03:25:34 +0900

    탈주범 검거 주석

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -16,6 +16,7 @@ public class Solution {
     static int []dx = {0,0,-1,1};//상0 하1 좌2 우3
     static int []dy = {-1,1,0,0};
 
+    //현재 파이프가 연결된 나가는 방향과 들어갈 수 있는방향을 리스트화
     static int [][]outType = {{},{0,1,2,3},{0,1},{2,3},{0,3},{1,3},{1,2},{0,2}};
     static int [][]inType = {{},{0,1,2,3},{0,1},{2,3},{1,2},{0,2},{0,3},{1,3}};
     static int [][]map;
@@ -55,10 +56,12 @@ public class Solution {
                 int y = point.y;
                 int d = point.d;
 
+                //최대거리 도달시 stop
                 if(d == L) {
                     continue;
                 }
 
+                //먼저 나갈 수 있는지 체크
                 for(int i : outType[map[y][x]]) {
                     int nx = x + dx[i];
                     int ny = y + dy[i];
@@ -67,7 +70,7 @@ public class Solution {
                         continue;
                     }
 
-
+                    //들어갈 수 있는지 체크
                     for(int j: inType[map[ny][nx]]) {
                         if(i == j && !visited[ny][nx]) {
                             visited[ny][nx] = true;
