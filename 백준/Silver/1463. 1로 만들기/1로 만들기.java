9b3833e499ce2672144cf832515458541ddd90fd
박하민 <129127559+wavewwave20@users.noreply.github.com>
2024-09-04 15:51:18 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18132 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        
+        int[]dp = new int[n+1];
+
+        for(int i = 2; i<=n; i++) {
+            dp[i] = dp[i-1]+1;
+            if(i%2 == 0) {
+                dp[i] = dp[i] < dp[i/2]+1 ? dp[i] : dp[i/2]+1;
+            }
+            if(i%3 == 0) {
+                dp[i] = dp[i] < dp[i/3]+1 ? dp[i] : dp[i/3]+1;
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
