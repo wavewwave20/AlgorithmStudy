58da2382df18da33f734039059e83068e87125af
박하민 <129127559+wavewwave20@users.noreply.github.com>
2024-09-18 13:32:55 +0900

    [Silver III] Title: N과 M (8), Time: 148 ms, Memory: 18416 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,109 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Main
+ */
+public class Main {
+    static int N,M;
+    static int[] arr;
+    // static ArrayList<String> list;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        sb = new StringBuilder();
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ;i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        //N과M 5
+        // permutation(0, new int[M], new boolean[N]);
+
+        //N과M 6
+        //combination(0,0, new int[M]);
+
+        //N과M 7
+        //permutationWithRepetition(0, new int[M], new boolean[N]);
+
+        //N과M 8
+        combinationWithRepetition(0,0, new int[M]);
+
+        System.out.println(sb.toString());
+        
+    }
+
+    
+    static void combination(int cnt, int start, int [] result) {
+        if(cnt == M) {
+            for(int i = 0; i<M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i<N; i++) {
+            result[cnt] = arr[i];
+            combination(cnt+1, i+1, result);
+        }
+    }
+
+    static void combinationWithRepetition(int cnt, int start, int [] result) {
+        if(cnt == M) {
+            for(int i = 0; i<M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i<N; i++) {
+            result[cnt] = arr[i];
+            combinationWithRepetition(cnt+1, i, result);
+        }
+    }
+
+    
+    static void permutation(int cnt, int[] result, boolean[]visited) {
+        if(cnt == M) {
+            for(int i = 0; i<M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i<N; i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            result[cnt] = arr[i];
+            permutation(cnt+1, result, visited);
+            visited[i] = false;
+        }
+    }
+
+    static void permutationWithRepetition(int cnt, int[] result, boolean[]visited) {
+        if(cnt == M) {
+            for(int i = 0; i<M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i<N; i++) {
+            result[cnt] = arr[i];
+            permutationWithRepetition(cnt+1, result, visited);
+        }
+    }
+}
