b862d9ebe840d8ecac03e6645f91ad01f58f0963
박하민 <129127559+wavewwave20@users.noreply.github.com>
2024-11-20 22:46:46 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    static public void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        
+        
+        System.out.println(a+b);
+
+    }
+}
