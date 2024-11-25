58193c40ca85d20e0378c5c0ec7801ab00c01b45
박하민 <129127559+wavewwave20@users.noreply.github.com>
2024-11-25 23:13:46 +0900

    [Bronze V] Title: 연세대학교, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    static public void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if(n == 0) {
+            System.out.println("YONSEI");
+        }else if(n==1){
+            System.out.println("Leading the Way to the Future");
+        }
+        
+        
+
+    }
+}
