05b0007f46ecd090add0ff42dba4cd2c377280c4
박하민 <129127559+wavewwave20@users.noreply.github.com>
2024-12-03 02:07:35 +0900

    [Bronze V] Title: 제리와 톰, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static public void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(b-a).append(" ").append(b);
+        
+        System.out.println(sb);
+    }
+}
